63a2b6a535450221b8e7ee06c250f2daf41ccdaf
Noibisjunior <noibisjunior22@gmail.com>
2023-01-26 17:24:10 +0100

    tagss

--- components/layout.js
+++ components/layout.js
@@ -2,13 +2,25 @@ import Footer from "./footer"
 import Nav from "./nav"
 
 const Layout = ({ children }) =>{
-    return(
-        <div className="content">
-           <Nav />
-           { children }
-           <Footer />
-        </div>
-    )
+    return (
+      <>
+        <head>
+          <script
+            type="text/javascript"
+            async="async"
+            src="//pl18342317.highcpmrevenuenetwork.com/78/c8/bc/78c8bc6
+    79de968d3ea881edfe3d9c47c.js"
+          ></script>
+        </head>
+        <body>
+          <div className="content">
+            <Nav />
+            {children}
+            <Footer />
+          </div>
+        </body>
+      </>
+    );
 }
 
 export default Layout;
